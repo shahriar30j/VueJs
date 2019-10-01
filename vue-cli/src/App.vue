913e110100dab75e7dab89e36e05d1e06b373ad2
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-10-01 17:08:49 +0600

    Section 7
    INtroduction to Components

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,11 +1,30 @@
 <template>
-  <h1>Hello World!</h1>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-servers></app-servers>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
-export default {
- 
-};
+    import Header from './components/Shared/Header';
+    import Footer from './components/Shared/Footer';
+    import Servers from './components/Server/Servers';
+    import ServerDetails from './components/Server/ServerDetails';
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-servers': Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
+    }
 </script>
 
 <style>
