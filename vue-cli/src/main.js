d8eb67b8a9e512b63b48d75ee47e9ad9995ad1a5
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-10-07 02:18:32 +0600

    VueJs part 8
    Communicating between components

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,5 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+export const eventBus = new Vue({
+  methods: {
+    changeAge(age) {
+      this.$emit('ageWasEdited', age);
+    }
+  }
+});
 
 new Vue({
   el: '#app',
