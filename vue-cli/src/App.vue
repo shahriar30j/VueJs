f3c79a712487799e06b06fb889c97fd295bf7dbb
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-12-03 17:56:40 +0600

    section 13 (initial)
    Filter and mixing (filters)

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -5,27 +5,22 @@
                 <h1>Filters & Mixins</h1>
                 <p>{{ text | toUppercase | to-lowercase }}</p>
                 <hr>
-                <button @click="fruits.push('Berries')">Add New Item</button>
                 <input v-model="filterText">
                 <ul>
                     <li v-for="fruit in filteredFruits">{{ fruit }}</li>
                 </ul>
-                <hr>
-                <app-list></app-list>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import List from './List.vue';
-    import { fruitMixin } from './fruitMixin';
-
     export default {
-        mixins: [fruitMixin],
         data() {
             return {
-                text: 'Hello there!'
+                text: 'Hello there!',
+                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+                filterText: ''
             }
         },
         filters: {
@@ -33,8 +28,12 @@
                 return value.toUpperCase();
             }
         },
-        components: {
-            appList: List
+        computed: {
+            filteredFruits() {
+                return this.fruits.filter((element) => {
+                    return element.match(this.filterText);
+                });
+            }
         }
     }
 </script>
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -5,12 +5,6 @@ Vue.filter('to-lowercase', function(value) {
     return value.toLowerCase();
 });
 
-Vue.mixin({
-    created() {
-        console.log('Global Mixin - Created Hook');
-    }
-});
-
 new Vue({
   el: '#app',
   render: h => h(App)
