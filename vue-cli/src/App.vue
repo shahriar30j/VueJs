5ea6f8a74a52642f5480754c1d5cdfd4f3229c8a
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-12-03 17:20:59 +0600

    section 13
    filters and mixing

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -2,56 +2,39 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Built-in Directives</h1>
-                <p v-text="'Some Text'"></p>
-                <p v-html="'<strong>Some strong text</strong>'"></p>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Custom Directives</h1>
-                <p v-highlight:background.delayed="'red'">Color this</p>
-                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color this, too</p>
+                <h1>Filters & Mixins</h1>
+                <p>{{ text | toUppercase | to-lowercase }}</p>
+                <hr>
+                <button @click="fruits.push('Berries')">Add New Item</button>
+                <input v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+                </ul>
+                <hr>
+                <app-list></app-list>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import List from './List.vue';
+    import { fruitMixin } from './fruitMixin';
+
     export default {
-        directives: {
-            'local-highlight': {
-                bind(el, binding, vnode) {
-                    var delay = 0;
-                    if (binding.modifiers['delayed']) {
-                        delay = 3000;
-                    }
-                    if (binding.modifiers['blink']) {
-                        let mainColor = binding.value.mainColor;
-                        let secondColor = binding.value.secondColor;
-                        let currentColor = mainColor;
-                        setTimeout(() => {
-                            setInterval(() => {
-                                currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
-                                if (binding.arg == 'background') {
-                                    el.style.backgroundColor = currentColor;
-                                } else {
-                                    el.style.color = currentColor;
-                                }
-                            }, binding.value.delay);
-                        }, delay);
-                    } else {
-                        setTimeout(() => {
-                            if (binding.arg == 'background') {
-                                el.style.backgroundColor = binding.value.mainColor;
-                            } else {
-                                el.style.color = binding.value.mainColor;
-                            }
-                        }, delay);
-                    }
-                }
+        mixins: [fruitMixin],
+        data() {
+            return {
+                text: 'Hello there!'
             }
+        },
+        filters: {
+            toUppercase(value) {
+                return value.toUpperCase();
+            }
+        },
+        components: {
+            appList: List
         }
     }
 </script>
